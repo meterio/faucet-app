3796da9fc510fe2deac7e2a0821ccf5d7a661e47
unknown <pappiahkubi1@gmail.com>
2021-05-07 11:01:55 +0000

    upgrade google reCaptcha to v3

--- src/controllers/tap.controller.ts
+++ src/controllers/tap.controller.ts
@@ -78,7 +78,7 @@ class TapController implements Controller {
       return next(new InvalidCaptchaException());
     }
     console.log(captcha);
-    const secret = '6Lehu6oaAAAAAINvGK3O0stKztQeuNlJbWBPNrLR';
+     const secret = '6LfZJsoaAAAAANvqUWigTkF39Y6DkDW752Vs0TUP';
     const res = await axios.post(
       `https://www.google.com/recaptcha/api/siteverify?secret=${secret}&response=${captcha}`
     );
