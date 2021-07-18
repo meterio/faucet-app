9ad4734fc8613fff2f44daadc79ee830a17b4aa8
Simon Zhang <zhanghan.simon@gmail.com>
2021-07-18 22:08:39 +0800

    add rebate address

--- src/services/transaction.service.ts
+++ src/services/transaction.service.ts
@@ -1,47 +1,38 @@
 import axios from 'axios';
-const baseUrl =  "https://api.meter.io:8000/api/accounts";
+const baseUrl = 'https://api.meter.io:8000/api/accounts';
 const while_list = [
-'0x1c4b70a3968436b9a0a9cf5205c787eb81bb558c',
-'0x0d0707963952f2fba59dd06f2b425ace40b492fe',
-'0xc1a76f84d977e8d424a8eb09ce6adf029d38b91d',
-'0x6f04787447975b40d29611833711117ed9de155f'
+  '0x1c4b70a3968436b9a0a9cf5205c787eb81bb558c',
+  '0x0d0707963952f2fba59dd06f2b425ace40b492fe',
+  '0xc1a76f84d977e8d424a8eb09ce6adf029d38b91d',
+  '0x6f04787447975b40d29611833711117ed9de155f',
+  '0x40df6f787bf8bd3fba3b2ef5a742ae0c993f1418', // rebate address
 ];
 
-const getTotalCount = async (address:string) => {
-    const res = await axios.get(
-       `${baseUrl}/${address}/txs`
-      );
-      return res.data.totalRows ? res.data.totalRows : 0;
-    
-}
+const getTotalCount = async (address: string) => {
+  const res = await axios.get(`${baseUrl}/${address}/txs`);
+  return res.data.totalRows ? res.data.totalRows : 0;
+};
 
-const getTransactions = async (address:string) => {
-    const totalRows = await getTotalCount(address);
-    const res = await axios.get(
-       `${baseUrl}/${address}/txs?page=${totalRows}&limit=1`
-      );
-      return res;
-    
-}
+const getTransactions = async (address: string) => {
+  const totalRows = await getTotalCount(address);
+  const res = await axios.get(
+    `${baseUrl}/${address}/txs?page=${totalRows}&limit=1`
+  );
+  return res;
+};
 
+const isWhiteListVerified = async (address: string) => {
+  var hasAccess = false;
+  const allTransactions = await getTransactions(address);
+  if (allTransactions.data.txSummaries.length) {
+    var lastOrigin = allTransactions.data.txSummaries[0].origin;
+    console.log(lastOrigin);
 
-
-
-
- const isWhiteListVerified = async (address:string) => {
-    var hasAccess = false
-    const allTransactions = await getTransactions(address);
-    if(allTransactions.data.txSummaries.length){
-    
-        var lastOrigin = allTransactions.data.txSummaries[0].origin
-        console.log(lastOrigin)
-        
-        if(while_list.includes(lastOrigin)){
-            hasAccess = true
-        }
+    if (while_list.includes(lastOrigin)) {
+      hasAccess = true;
     }
-    return hasAccess
+  }
+  return hasAccess;
+};
 
-}
-
-export default isWhiteListVerified
+export default isWhiteListVerified;
