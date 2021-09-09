7a4dc025b0132205963d35782f98432ac6337875
Simon Zhang <zhanghan.simon@gmail.com>
2021-09-09 23:29:48 +0800

    add meter bridge erc20 handler to white list

--- src/services/transaction.service.ts
+++ src/services/transaction.service.ts
@@ -6,6 +6,7 @@ const while_list = [
   '0xc1a76f84d977e8d424a8eb09ce6adf029d38b91d',
   '0x6f04787447975b40d29611833711117ed9de155f',
   '0x40df6f787bf8bd3fba3b2ef5a742ae0c993f1418', // rebate address
+  '0x60f1abaa3ed8a573c91c65a5b82aec4bf35b77b8', // meter bridge erc20 handler
 ];
 
 const getTotalCount = async (address: string) => {
